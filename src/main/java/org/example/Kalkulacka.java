4d69e8d42b1d926cf1f22aae0ddd9a950bdf5d3d
Mikoláš Pešek <35407839+KosacekCZ@users.noreply.github.com>
2023-05-07 14:51:12 +0200

    Vytvoření kalkulačky

---
+++ src/main/java/org/example/Kalkulacka.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Kalkulacka {
+
+    
+
+    public static double secti (double x, double y) {
+        return x + y;
+    }
+
+    public static double vynasob (double x, double y) {
+        return x * y;
+    }
+}
